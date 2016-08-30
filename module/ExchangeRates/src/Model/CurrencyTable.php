af8db95d0b2be248611e5a8c21db38168e44d83d
Johan Lorentzon <johanlorentzon@zoho.com>
2016-08-30 11:43:38 +0200

    currency codes didn't show up

--- module/ExchangeRates/src/Model/CurrencyTable.php
+++ module/ExchangeRates/src/Model/CurrencyTable.php
@@ -3,6 +3,7 @@ namespace ExchangeRates\Model;
 
 use RuntimeException;
 use Zend\Db\TableGateway\TableGatewayInterface;
+use Zend\Db\Sql\Select;
 
 class CurrencyTable
 {
@@ -15,7 +16,9 @@ class CurrencyTable
 
 	public function fetchAll()
 	{
-		return $this->tableGateway->select();
+		return $this->tableGateway->select(function (Select $select) {
+			$select->order('currency ASC');
+		});
 	}
 
 	public function getCurrency($code)
